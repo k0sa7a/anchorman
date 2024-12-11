e55120a7162dbd37078fa0ccf3e5e0c9f96d2eb9
Stefan Filev <stefan.s.filev@gmail.com>
2024-12-11 10:50:05 +0000

    added caption generation functionality as well

--- src/anchorman/api/chatgpt.clj
+++ src/anchorman/api/chatgpt.clj
@@ -15,17 +15,17 @@
         content (:content article)
         clj-payload {"model" "gpt-4o-mini"
                      "messages" [{"role" "system"
-                                  "content" "You are a professional news anchor named News Bot. Your task is to deliver engaging scripts for news segments.
-"
+                                  "content" "You are a professional news anchor named Nexus at The Cyber Bulletin. Your task is to deliver engaging scripts for news segments."
                                   }
                                  {"role" "user"
                                   "content" (format "You will receive the title, description, source, and content of a news article. Based on this information:
                                                        - Write a professional and engaging script as if delivering it on air.
-                                                       - Limit the script to approximately 150-200 words (about 1-minute reading time).
+                                                       - Reference the source for the news.
+                                                       - Limit the script to approximately 140 words (around 50 seconds reading time).
                                                        - Do not include any additional text or explanation; output only the script.
                                                        - Do not include any placeholders of any kind, just text as it is to be read.
                                                        - Do not include reference to the time of day when the news are being presented.
-                                                       - Reference the source of the news.
+                                                       - Mention your name (Nexus) and The Cyber Bulletin
                                                      Title: %s\n
                                                      Description: %s\n
                                                      Source: %s\n
@@ -61,3 +61,42 @@
                                                                              :body payload
                                                                          :as :byte-array})]
     response))
+
+
+(defn caption-generate-payload [article]
+
+  (let [title (:title article)
+        description (:description article)
+        source (:name (:source article))
+        content (:content article)
+        clj-payload {"model" "gpt-4o-mini"
+                     "messages" [{"role" "system"
+                                  "content" "You are a professional social media manager working for 'The Cyber Bulletin.' Your task is to prepare a short caption for a reel or post based on specific news articles."}
+                                 {"role" "user"
+                                  "content" (format "Below is the information for a specific news article. Based on this:
+                                                       - Write a professional and engaging caption for a social media reel or post that directly relates to this article.
+                                                       - Focus on the key points of the article to intrigue viewers and encourage them to engage.
+                                                       - Limit the caption to 2-3 sentences, focusing on the title and description.
+                                                       - Do not include generic statements or placeholders; ensure the caption directly reflects the article’s content.
+                                                       - At the end of the caption, include the hashtags: #ScienceNews #Science #TheCyberBulletin.
+
+                                                     Title: %s
+                                                     Description: %s
+                                                     Source: %s
+                                                     Content: %s" title description source content)}]}]
+    (json/generate-string clj-payload)))
+
+(defn ask-chatgpt-for-caption [article]
+  (println "Calling ask-chatgpt-for-caption...")
+  (let [payload (caption-generate-payload article)
+        response-str (:body (client/post "https://api.openai.com/v1/chat/completions" {:headers {"Authorization" (str "Bearer " chatgpt-api-key)
+                                                                                                 "Content-Type" "application/json"}
+                                                                                       :body payload}))
+        response (json/parse-string response-str true)
+        caption (-> response
+                    :choices
+                    first
+                    :message
+                    :content)]
+
+    caption))
--- src/anchorman/scripts.clj
+++ src/anchorman/scripts.clj
@@ -3,9 +3,10 @@
             [anchorman.api.chatgpt :as chatgpt]
             [clojure.java.io :as io]))
 
+(def article-to-use (gnews/get-article) )
 
 (defn generate-script []
-  (let [article (gnews/get-article)]
+  (let [article article-to-use]
 
     (anchorman.api.chatgpt/ask-chatgpt-for-script article)))
 
@@ -16,6 +17,9 @@
   (with-open [out (io/output-stream file-path)]
     (io/copy binary-data out)))
 
+(defn save-text-to-file [string-data file-path]
+  (with-open [out (io/output-stream file-path)]
+    (.write out (.getBytes string-data "UTF-8"))))
 
 (defn generate-audio []
   (let [script (generate-script)
@@ -23,10 +27,20 @@
         file-path (str "output/audio-" (java.time.LocalDateTime/now) ".mp3")
         sanitized-timestamp (clojure.string/replace file-path #":" "-")
         audio (:body response)]
-
+    (print script)
     (save-audio-to-file audio sanitized-timestamp)
     sanitized-timestamp))
 
 
 
+(defn generate-caption []
+   (let [article (gnews/get-article)
+        caption-string (anchorman.api.chatgpt/ask-chatgpt-for-caption article)
+        file-path (str "output/caption-" (java.time.LocalDateTime/now) ".txt")
+        sanitized-timestamp (clojure.string/replace file-path #":" "-")]
+    (save-text-to-file caption-string sanitized-timestamp)
+    sanitized-timestamp)
+  )
+
 (generate-audio)
+(generate-caption)
